fe7c533fae2cb5ad09f31a66a36fd329650784c3
MananJadam <mananjadam33@gmail.com>
2026-07-05 20:04:35 +0530

    Time: 15 ms (9.06%), Space: 27.1 MB (7.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,29 +2,42 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        list<int> lt;
+        // list<int> lt;
 
-        for (int i = 0; i < n; i++) {
+        // for (int i = 0; i < n; i++) {
 
-            if (lt.size() == 0 || lt.front() != nums[i]) {
-                int cnt = 0;
+        //     if (lt.size() == 0 || lt.front() != nums[i]) {
+        //         int cnt = 0;
 
-                for (int j = 0; j < n; j++) {
-                    if (nums[i] == nums[j]) {
-                        cnt++;
-                    }
-                }
+        //         for (int j = 0; j < n; j++) {
+        //             if (nums[i] == nums[j]) {
+        //                 cnt++;
+        //             }
+        //         }
 
-                if (cnt > n / 3) {
-                    lt.push_back(nums[i]);
-                }
+        //         if (cnt > n / 3) {
+        //             lt.push_back(nums[i]);
+        //         }
+        //     }
+
+        //     if (lt.size() == 2) {
+        //         break;
+        //     }
+        // }
+
+        // return vector<int>(lt.begin(), lt.end());
+
+        vector <int> ls;
+        map <int,int> mpp;
+        int mini=(int)(n/3)+1;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+            if(mpp[nums[i]]==mini){
+                ls.push_back(nums[i]);
             }
-
-            if (lt.size() == 2) {
-                break;
-            }
+            if(ls.size()==2)break;
         }
+        return ls;
 
-        return vector<int>(lt.begin(), lt.end());
     }
 };
