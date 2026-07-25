df037b842719014822f5fa5e5b3d64ab4f25347d
MananJadam <mananjadam33@gmail.com>
2026-07-26 00:21:58 +0530

    Time: 55 ms (6.49%), Space: 8.6 MB (14.81%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int ans=1;
+        if(x==0)return 0;
+        for(int i=1; i<=x; i++ ){
+            if(i<=x/i){
+                ans=i;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
