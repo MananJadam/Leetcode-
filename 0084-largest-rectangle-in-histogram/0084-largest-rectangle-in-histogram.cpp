e45dafba192d6ab5fe3d8bbd83dfac35949289f5
MananJadam <mananjadam33@gmail.com>
2026-08-08 21:43:51 +0530

    Time: 27 ms (37.85%), Space: 81.4 MB (71.52%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,57 +1,20 @@
 class Solution {
 public:
-    vector<int>pse(vector<int>&arr){
-        int n=arr.size();
-        vector<int>pse(n);
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty()&& arr[st.top()]>=arr[i]){
+    int largestRectangleArea(vector<int>& nums) {
+        stack<int> st;
+        int area=0;
+        int n = nums.size();
+        for(int i=0;i<=nums.size();i++){
+            int curr= i==n?0:nums[i];
+            while(!st.empty() && nums[st.top()]>curr){
+                int index=st.top();
                 st.pop();
-            }
-            if(st.empty()){
-                pse[i]=-1;
-            }
-            else{
-                pse[i]=st.top();
-            }
-            st.push(i);
-
-        
-        }
-        return pse;
-    }
-    vector<int>nse(vector<int>&arr){
-        int n=arr.size();
-        stack<int>st;
-        vector<int>nse(n);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&& arr[st.top()]>=arr[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                nse[i]=n;
-
-            }
-            else{
-                nse[i]=st.top();
+                int width=st.empty()?i:i-st.top()-1;
+                area=max(area,width*nums[index]);
             }
             st.push(i);
             
         }
-        return nse;
-    }
-    int largestRectangleArea(vector<int>& heights) {
-
-        vector<int> psee = pse(heights);
-        vector<int> nsee = nse(heights);
-        int maxi=0;
-        for(int i = 0; i < heights.size(); i++)
-        {
-            int width = nsee[i] - psee[i] - 1;
-            int area = heights[i] * width;
-
-            maxi = max(maxi, area);
-        }
-        return maxi;  
+        return area;
     }
 };
